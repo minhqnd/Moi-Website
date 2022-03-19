37ff029e7150a60a1e369c90e91e3bf94d9c8a7c
stromez <zemorts@gmail.com>
2022-03-19 22:32:06 +0700

    Add files via upload

--- script/ip.js
+++ script/ip.js
@@ -5,23 +5,63 @@ var dateTime = time + ' ' + date;
 var app;
 var db = firebase.database();
 var ip = document.getElementById('ip');
+var city = document.getElementById('city');
 var region = document.getElementById('region');
 var country = document.getElementById('country');
+var loc = document.getElementById('loc');
+var postal = document.getElementById('postal');
 var org = document.getElementById('org');
-var time = document.getElementById('time');
+var timezone = document.getElementById('timezone');
+var checktime = document.getElementById('checktime');
+var showmap = document.getElementById('showmap');
+var map = L.map(showmap);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {   
+}).addTo(map);
 
+if (location.hash != '') {
+	console.log(location.hash.slice(1))
+	$.getJSON('https://ipinfo.io/'+ location.hash.slice(1) +'?token=03f8459a537072', function(data) {
+	JSON.stringify(data, null, 2);
+	show(data)
+	savedb(data);
+}).fail(function(cc) {
+    if (cc.status == 404) {
+        alert('Wrong ip\nPlease provide a valid IP address');
+    } else {
+        alert('Error');
+    }
+});
+} else {
 $.getJSON('https://ipinfo.io/json', function(data) {
-	var json = JSON.parse(JSON.stringify(data, null, 2));
-	ip.innerHTML = `Your ip is: <b>${json.ip}</b> `
-	region.innerHTML = `Region: <i>${json.region}</i> `
-	country.innerHTML = `Country: <i>${json.country}</i> `
-	org.innerHTML = `Org: <i>${json.org}</i> `
-	time.innerHTML = `Time check: <b>${dateTime}</b> `
-		db.ref('ip/' + dateTime).set({
-			ip: json.ip,
-			region: json.region,
-			country: json.country,
-			org: json.org,
-			useragent: navigator.userAgent
-        })
+	JSON.stringify(data, null, 2);
+	show(data)
+	savemydb(data);
 })
+}
+
+function show(data) {
+	ip.innerHTML = `Your ip is: <b>${data.ip}</b>`
+	city.innerHTML = `City: <i>${data.city}</i>`
+	region.innerHTML = `Region: <i>${data.region}</i>`
+	country.innerHTML = `Country: <i>${data.country}</i>`
+	loc.innerHTML = `Loc: <i>${data.loc}</i>`
+	postal.innerHTML = `Postal: <i>${data.postal}</i>`
+	org.innerHTML = `Org: <i>${data.org}</i>`
+	timezone.innerHTML = `Timezone: <i>${data.timezone}</i>`
+	checktime.innerHTML = `Time check: <b>${dateTime}</b>`
+	var target = L.latLng(data.loc.split(',')[length], data.loc.split(',')[length+1]);
+	map.setView(target, 14);
+	L.marker(target).addTo(map);
+}
+
+function savemydb(data) {
+	    db.ref('ip/' + date + '/' + time).set({
+			data	
+        })
+}
+
+function savedb(data) {
+	    db.ref('ip/check/' + date + '/' + time).set({
+			data	
+        })
+}
