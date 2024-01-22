bf83244da6b0a8fc8a4945e2dc43027a8cb87d20
Quang Minh <zemorts@gmail.com>
2024-01-23 01:39:41 +0700

    Update URL and API endpoint

--- url/script/manage.js
+++ url/script/manage.js
@@ -1,3 +1,4 @@
+var apiUrl = "https://stromez-ed239-default-rtdb.asia-southeast1.firebasedatabase.app/public/shortenurl/"
 const erbox = document.getElementById('erbox')
 const keyinput = document.getElementById('key')
 const output = document.getElementById('output')
@@ -10,7 +11,12 @@ const getkey = () => {
 	const key = document.getElementById('key').value
 	return key
 }
-	keyinput.value = ''
+keyinput.value = ''
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 const check = async () => {
@@ -21,24 +27,45 @@ const check = async () => {
 		erbox.innerHTML = 'invalid alias'
 	} else {
 		if (cre.test(keyinput.value)) {
-			firebase.database().ref("shortenurl/" + key).once("value", snapshot=>{
-                if (snapshot.exists()) {
-                    console.log("found!");
-                    output.style.display = 'block'
-					erbox.style.display = 'none'
-					console.log(snapshot.val().url)
-					urlcc.value = snapshot.val().url
-					time.innerHTML = snapshot.val().time
-					click.innerHTML = 'click: ' + snapshot.val().click
-                    return true;
-                } else {
-					output.style.display = 'none'
-					erbox.style.display = 'block'
-					console.log('not found')
+			fetch(apiUrl + key + ".json", requestOptions)
+				.then(response => response.json())
+				.then(result => {
+					console.log(result);
+					if (result == null) {
+						output.style.display = 'none'
+						erbox.style.display = 'block'
+						erbox.innerHTML = 'alias not found'
+					} else {
+						output.style.display = 'block'
+						erbox.style.display = 'none'
+						urlcc.value = result.url
+						time.innerHTML = result.time
+						click.innerHTML = 'click: ' + result.click
+						return true;
+					}
+				})
+				.catch(error => {
+					console.log('Error:', error);
+				});
 
-					erbox.innerHTML = 'alias not found'
-                }
-            })
+			// firebase.database().ref("shortenurl/" + key).once("value", snapshot => {
+			// 	if (snapshot.exists()) {
+			// 		console.log("found!");
+			// 		output.style.display = 'block'
+			// 		erbox.style.display = 'none'
+			// 		console.log(snapshot.val().url)
+			// 		urlcc.value = snapshot.val().url
+			// 		time.innerHTML = snapshot.val().time
+			// 		click.innerHTML = 'click: ' + snapshot.val().click
+			// 		return true;
+			// 	} else {
+			// 		output.style.display = 'none'
+			// 		erbox.style.display = 'block'
+			// 		console.log('not found')
+
+			// 		erbox.innerHTML = 'alias not found'
+			// 	}
+			// })
 		} else {
 			keyinput.value = ''
 			status.innerHTML = 'delete'
@@ -48,4 +75,10 @@ const check = async () => {
 	}
 }
 dbtn.addEventListener('click', check)
+keyinput.addEventListener("keyup", function (event) {
+	if (event.keyCode === 13) {
+		event.preventDefault();
+		dbtn.click();
+	}
+});
 
